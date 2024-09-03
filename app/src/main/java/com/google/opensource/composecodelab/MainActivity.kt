6f3f4cd8532698a81854d742d19df45bac890b66
leeeha <jxlhe46@gmail.com>
2024-09-03 21:12:57 +0900

    [ui] WaterCounter 구현

--- app/src/main/java/com/google/opensource/composecodelab/MainActivity.kt
+++ app/src/main/java/com/google/opensource/composecodelab/MainActivity.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,29 +27,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeCodelabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        Spacer(
+                            modifier = Modifier.windowInsetsTopHeight(WindowInsets.statusBars)
+                        )
+                        WellnessScreen()
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeCodelabTheme {
-        Greeting("Android")
-    }
+private fun WellnessScreenPreview() {
+    WellnessScreen()
 }
---
+++ app/src/main/java/com/google/opensource/composecodelab/WaterCounter.kt
@@ -0,0 +1,89 @@
+package com.google.opensource.composecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+/**
+ * State에 따라 UI에 표시되는 항목이 결정된다.
+ * 상태는 존재하고, 이벤트는 발생한다.
+ *
+ * Composition: Composable 실행에 따라 Compose가 빌드한 UI
+ * Recomposition: 이벤트 발생에 따라 변경된 State를 UI에 반영하기 위해 Composable을 다시 실행하는 것
+ *
+ * remember: 컴포지션에 저장된 객체 (리컴포지션 발생해도 상태 유지시킴)
+ * */
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableIntStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10,
+        ) {
+            Text("Add one")
+        }
+    }
+}
+
+/**
+ * State hoisting의 중요한 속성
+ * 단일 소스 저장소: 상태를 복제하는 대신 컴포저블의 호출자로 위치를 끌어올렸기 때문에 소스 저장소가 하나만 있다. (버그 방지에 도움)
+ * 공유 가능: 끌어올린 상태를 여러 컴포저블과 공유할 수 있다.
+ * 가로채기 가능: Stateless 컴포저블의 호출자는 상태를 변경하기 전에 이벤트를 건너뛸 것인지 결정할 수 있다.
+ * 분리 가능: Stateless 컴포저블의 상태는 어디에든(예: ViewModel) 저장할 수 있다.
+ *
+ * 상태 호이스팅의 일반적인 패턴
+ * value: T - 표시할 현재 값
+ * onValueChange: (T) -> Unit - 새로운 값 T로 상태가 변경되도록 요청하는 이벤트
+ * */
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(
+            onClick = onIncrement,
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text("Add one")
+        }
+    }
+}
+
+/**
+ * State를 어느 위치로 끌어올려야 하는지
+ *
+ * 상태는 적어도 그 상태를 사용하는 모든 컴포저블의 가장 낮은 공통 상위 요소로 끌어올려야 합니다. (get)
+ * 상태는 최소한 변경될 수 있는 가장 높은 수준으로 끌어올려야 합니다. (set)
+ * 두 상태가 동일한 이벤트에 대한 응답으로 변경되는 경우 두 상태는 동일한 수준으로 끌어올려야 합니다.
+ *
+ * 충분히 높은 수준으로 상태를 끌어올리지 않으면, UDF를 따르기 어려울 수 있다.
+ * */
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+    StatelessCounter(
+        count = count,
+        onIncrement = { count++ },
+        modifier = modifier
+    )
+}
