f9ef8eb4855f31b7d193ffc4a2764087b2373fa6
leeeha <jxlhe46@gmail.com>
2024-09-03 14:16:52 +0900

    [ui] align your body element 구현

--- app/src/main/java/com/google/opensource/composecodelab/MainActivity.kt
+++ app/src/main/java/com/google/opensource/composecodelab/MainActivity.kt
@@ -4,14 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.opensource.composecodelab.ui.theme.ComposeCodelabTheme
+
+/**
+ * Column : 수평 방향의 정렬
+ * Start, End, CenterHorizontally
+ *
+ * Row : 수직 방향의 정렬
+ * Top, Bottom, CenterVertically
+ *
+ * Box : 수평, 수직 방향의 정렬
+ * TopStart, TopCenter, TopEnd
+ * CenterStart, Center, CenterEnd
+ * BottomStart, BottomCenter, BottomEnd
+ *
+ * */
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +52,74 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeCodelabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(id = R.string.placeholder_search))
+        },
         modifier = modifier
+            .fillMaxWidth()
+            .heightIn(56.dp)
     )
+}
+
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawableResId: Int,
+    @StringRes stringResId: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = drawableResId),
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(id = stringResId),
+            modifier = modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeCodelabTheme {
-        Greeting("Android")
-    }
+private fun SearchBarPreview() {
+    SearchBar()
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+private fun AlignYourBodyElementPreview() {
+    AlignYourBodyElement(
+        stringResId = R.string.ab1_inversions,
+        drawableResId = R.drawable.ab1_inversions,
+        modifier = Modifier.padding(8.dp)
+    )
 }
