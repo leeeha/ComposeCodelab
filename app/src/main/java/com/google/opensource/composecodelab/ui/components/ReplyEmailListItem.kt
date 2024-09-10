666259727c56c52e85548fd94755240d2cca0bd1
leeeha <jxlhe46@gmail.com>
2024-09-10 16:54:31 +0900

    [ui] variant 사용하여 텍스트 강조도 낮추기

--- app/src/main/java/com/google/opensource/composecodelab/ui/components/ReplyEmailListItem.kt
+++ app/src/main/java/com/google/opensource/composecodelab/ui/components/ReplyEmailListItem.kt
@@ -85,7 +85,8 @@ fun ReplyEmailListItem(
                     )
                     Text(
                         text = email.createdAt,
-                        style = MaterialTheme.typography.labelMedium
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
                 }
                 IconButton(
@@ -110,6 +111,7 @@ fun ReplyEmailListItem(
                 text = email.body,
                 maxLines = 2,
                 style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
                 overflow = TextOverflow.Ellipsis
             )
         }
